993969d7b0653ddd77a8e157b9cee4c136e0f942
lokibeat <37983312+lokibeat@users.noreply.github.com>
2018-08-09 14:48:16 -0500

    for deployment except click listener

--- config/orm.js
+++ config/orm.js
@@ -1,13 +1,16 @@
 var connection = require("../config/connection.js");
 
+// helper function to insert "?" for query string
+function printQuestionMarks(num) {
+  var arr = [];
 
-// * In the `orm.js` file, create the methods that will execute the necessary MySQL commands in the controllers. These are the methods you will need to use in order to retrieve and store data in your database.
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
 
-//      * `selectAll()`
-//      * `insertOne()`
-//      * `updateOne()`
+  return arr.toString();
+}
 
-//    * Export the ORM object in `module.exports`.
 
 var orm = {
     all: function(tableInput, cb) {
